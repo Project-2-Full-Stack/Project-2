ca680ba0d84e3fcf16a42ab3ec0e983179761607
Alap Raval <alapraval@Alaps-MBP-2.T-mobile.com>
2021-07-10 10:46:01 -0500

    added svalues to radio buttons and selectors in js file

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,11 +4,15 @@ const signupFormHandler = async (event) => {
   const name = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+  const eatsMeat = document.querySelector('input[name="meat"]:checked').value || false;
+  const eatsDairy = document.querySelector('input[name="dairy"]:checked').value || false;
+  const eatsFish = document.querySelector('input[name="fish"]:checked').value || false;
+  const eatsGluten = document.querySelector('input[name="gluten"]:checked').value || false;
 
   if (name && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ name, email, password, eatsMeat, eatsDairy, eatsFish, eatsGluten}),
       headers: { 'Content-Type': 'application/json' },
     });
 
