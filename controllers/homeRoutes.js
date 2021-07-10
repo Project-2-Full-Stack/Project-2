985f61e2a4cd79248b68877f1077f782c01bcfcb
Tiffany Deakin <tdeakin93@gmail.com>
2021-07-10 15:36:58 -0500

    working homepage and more refactoring

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,25 +1,28 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Recipe, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    // Get all recipes and JOIN with user data
+    // TODO: update to JOIN what's in fridge
+    const recipeData = await Recipe.findAll(
+      // {
+      // // include: [
+      // //   {
+      // //     model: User,
+      // //     attributes: ['name'],
+      // //   },
+      // // ],
+      // }
+    );
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', {
-      projects,
+      recipes,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -27,21 +30,23 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+router.get('/recipe/:id', async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    const recipeData = await Recipe.findByPk(req.params.id,
+      // {
+      // include: [
+      //   {
+      //     model: User,
+      //     attributes: ['name'],
+      //   },
+      // ],
+      // }
+    );
 
-    const project = projectData.get({ plain: true });
+    const recipe = recipeData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
+    res.render('recipe', {
+      ...recipe,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -55,7 +60,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      // include: [{ model: Recipe }],
     });
 
     const user = userData.get({ plain: true });
@@ -71,10 +76,11 @@ router.get('/profile', withAuth, async (req, res) => {
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
+  // TODO: Eventually reroute to getRecipe page
+  // if (req.session.logged_in) {
+  //   res.redirect('/profile');
+  //   return;
+  // }
 
   res.render('login');
 });
